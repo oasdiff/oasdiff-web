833ed2b808c6581b04d9a56310609620650f0993
Reuven Harrison <rh@tufin.com>
2023-03-19 18:40:38 +0200

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {URLSearchParams} from '@angular/http';
+import {HttpClient, URLSearchParams} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
